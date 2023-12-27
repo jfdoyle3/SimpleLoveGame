9accbd8315d14e3e1cdf5042906d99d30b95783c
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-12-26 19:18:14 -0500

    cleaned up code

--- npc.lua
+++ npc.lua
@@ -5,11 +5,11 @@
 function Npc(name,life,attack,defend,str,dex,friend)
 	return {
 		name = name or "Nemesis",
-	life = life or 10,
-	attack = attack or 10,
-	defend = defend or 10,
-	str = str or 10,
-	dex = dex or 10,
-	friend=friend or false
+		life = life or 10,
+		attack = attack or 10,
+		defend = defend or 10,
+		str = str or 10,
+		dex = dex or 10,
+		friend=friend or false
 	}
 end
